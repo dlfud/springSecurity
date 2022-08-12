cfc0b00c328c12c7afab3ca392d88333788d9bde
unknown <99ysl@naver.com>
2022-08-12 11:16:16 +0900

    스프링 시큐리티

---
+++ src/main/java/com/example/spring/security/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.spring.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeRequests(authroize -> authroize.antMatchers("/user/**")
+                        .authenticated()
+                        .antMatchers("/manager/**")
+                        .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                        .antMatchers("/admin/**")
+                        .access("hasRole('ROLE_ADMIN')")
+                        .anyRequest().permitAll())
+                //인증 안된 페이지로 가면 로그인 페이지로 보냄
+                .formLogin()
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login")// login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
+                // /login 안만들어도 됨
+                .defaultSuccessUrl("/")
+                .and()
+                .build();
+    }
+}
---
+++ src/main/java/com/example/spring/security/controller/MainController.java
@@ -0,0 +1,65 @@
+package com.example.spring.security.controller;
+
+import com.example.spring.security.dao.UserRepository;
+import com.example.spring.security.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({" ","/"})
+    public String main(){
+        return "index";
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/manager")
+    @ResponseBody
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/loginForm")
+    public String login(){
+        return "/loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "/joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user){
+        System.out.println("user");
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user);
+        return "redirect:/loginForm";
+    }
+}
