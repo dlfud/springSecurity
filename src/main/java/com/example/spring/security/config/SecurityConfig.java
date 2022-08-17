67679f90ca7e768d3a408773bdd79fcefec616b8
unknown <99ysl@naver.com>
2022-08-17 10:05:51 +0900

    정보 접근

--- src/main/java/com/example/spring/security/config/SecurityConfig.java
+++ src/main/java/com/example/spring/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.spring.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured annotation 활성화, preAuthorize annotation 활성화
 public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder encodePwd() {
--- src/main/java/com/example/spring/security/controller/MainController.java
+++ src/main/java/com/example/spring/security/controller/MainController.java
@@ -3,6 +3,10 @@ package com.example.spring.security.controller;
 import com.example.spring.security.dao.UserRepository;
 import com.example.spring.security.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +23,23 @@ public class MainController {
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping({" ","/"})
-    public String main(){
+    public String main(@AuthenticationPrincipal UserDetails userDetails){
+        if(userDetails != null){
+            System.out.println(userDetails.getUsername());
+        }
         return "index";
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/info")
+    public @ResponseBody String info(){
+        return "개인정보";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/user_info_data")
+    public @ResponseBody String user_info_data() {
+        return "민감한 정보";
     }
 
     @GetMapping("/user")
